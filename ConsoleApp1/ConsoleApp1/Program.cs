5a36024b3645afabdc3f014f4cde0777b1141e41
Eirene Karunungan <eireneysabelkarunungan@gmail.com>
2026-06-17 22:56:12 +0800

    Added source code with 3 classes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+
+        while (true)
+        {
+            int choice = SelectionMenu.MainMenu();
+
+            switch (choice)
+            {
+                case 1:
+                    StudentManager.AddStudent();
+                    break;
+                case 2:
+                    StudentManager.ViewAllStudents();
+                    break;
+                case 3:
+                    StudentManager.ComputeAverageGrade();
+                    break;
+                case 4:
+                    StudentManager.FindHighestGrade();
+                    break;
+                case 5:
+                    Console.WriteLine("Exiting program...");
+                    Console.WriteLine("Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
+    }
+}
