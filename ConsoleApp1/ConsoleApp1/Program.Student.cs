85fc6260d31843062ed6e3111c6fce4b363cd1ab
eireneysabel <eireneysabelkarunungan@gmail.com>
2026-06-20 12:25:16 +0800

    Added methods

--- ConsoleApp1/ConsoleApp1/Program.Student.cs
+++ ConsoleApp1/ConsoleApp1/Program.Student.cs
@@ -1,17 +1,35 @@
 ﻿
     public class Student
     {
-        public string Name;
-        public double Grade1;
-        public double Grade2;
-        public double Grade3;
+        string Name;
+        double Grade1;
+        double Grade2;
+        double Grade3;
 
         public Student(string name, double grade1, double grade2, double grade3)
         {
             Name = name;
             Grade1 = grade1;
             Grade2 = grade2;
-            Grade3 = grade3;
+            Grade3 = grade3;    
         }
+
+    public string GetName()
+    {
+
+        return Name;
     }
+
+    public double[] GetGrades()
+    {
+
+
+        return [Grade1, Grade2, Grade3];
+    }
+
+    public double GetAverage()
+    {
+        return Grade1 + Grade2 + Grade3 / 3;
+    }
+}
 
